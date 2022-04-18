e480a3e9fa22935f6bddd027075e437852f4d611
Karlis Cudars <karlis.cudars1996@gmail.com>
2022-04-18 20:49:49 +0300

    Home show todays worst performing flashcards

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
+    if member_signed_in?
+      @todays_worst_performing_flashcards = current_member.
+        flashcards.
+        joins(:flashcard_answers).
+        select("flashcards.*").
+        select_answer_count.
+        select_wrong_answer_count.
+        where(
+          flashcard_answers: {
+            created_at: (Time.now - 1.day)..Time.now
+          }
+        ).
+        group("flashcards.id").
+        order("wrong_answer_count DESC").
+        limit(20)
+    end
   end
 end
--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -19,4 +19,20 @@ class Flashcard < ApplicationRecord
   belongs_to :member
   belongs_to :flashcard_category
   has_many :flashcard_answers, dependent: :destroy
+
+  def self.select_answer_count
+    joins(:flashcard_answers).
+      select("count(flashcard_answers.id) as answer_count")
+  end
+
+  def self.select_wrong_answer_count
+    joins(:flashcard_answers).
+      select("count(flashcard_answers.correct) filter (where flashcard_answers.correct = false) as wrong_answer_count")
+  end
+
+  # Can only be called after select_wrong_answer_count and select_answer_count
+  def wrong_answer_percentage
+    return 100 if !defined?(answer_count) || !defined?(wrong_answer_count)
+    (wrong_answer_count.to_f / answer_count.to_f * 100).to_i
+  end
 end
